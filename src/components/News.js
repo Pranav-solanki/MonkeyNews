63a649e3e38bf813e9109909cfe8586fe26e2550
Pranav solanki <github39645@gmail.com>
2025-08-07 09:27:37 +0530

    new change

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Newsitem from "./Newsitem";
 
 export default function News() {
   return (
     <div>
-      
+      <h2 className="text-center">MonkeyNews-Top headline</h2>
+      <div className="row">
+        <div className="col-md-4">
+          <Newsitem />
+        </div>
+        <div className="col-md-4">
+          <Newsitem />
+        </div>
+        <div className="col-md-4">
+          <Newsitem />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <Newsitem />
+        </div>
+        <div className="col-md-4">
+          <Newsitem />
+        </div>
+        <div className="col-md-4">
+          <Newsitem />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
