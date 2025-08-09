099c644140698ca3e1bfe56ac8ca5960573057b9
Pranav solanki <github39645@gmail.com>
2025-08-09 15:02:34 +0530

    new change

--- src/components/News.js
+++ src/components/News.js
@@ -5,14 +5,18 @@ import { useState, useEffect } from "react";
 
 export default function News() {
   const [article, setarticle] = useState([]);
+  const [pages, setPage] = useState(1);
+
   useEffect(() => {
     const fetchNews = async () => {
       try {
         const response = await fetch(
-          "https://newsdata.io/api/1/latest?apikey=pub_c0b163a1315d4364a6752ca770d9fa7b&country=in&prioritydomain=top"
+          `https://newsdata.io/api/1/latest?apikey=pub_c0b163a1315d4364a6752ca770d9fa7b&country=in&prioritydomain=top`
         );
         const data = await response.json();
+        console.log(data);
         setarticle(data.results);
+        setPage(data.nextPage);
       } catch (error) {
         console.error("API error:", error);
       }
@@ -20,29 +24,52 @@ export default function News() {
 
     fetchNews();
   }, []);
+
+ 
+  const handlenext = async () => {
+    try {
+      const response = await fetch(
+        `https://newsdata.io/api/1/latest?apikey=pub_c0b163a1315d4364a6752ca770d9fa7b&country=in&prioritydomain=top&page=${
+          pages
+        }`
+      );
+      const data = await response.json();
+      setarticle(data.results);
+      setPage(data.nextPage)
+    } catch (error) {
+      console.error("API error:", error);
+    }
+  };
   return (
-    <div>
-      <h2 className="text-center">MonkeyNews-Top headline</h2>
-      <div className="row">
-        {article.map((news, index) => (
-          <div
-            className="col-md-4 mb-4"
-            key={news.link}
-            style={{ height: "500px", overflow: "hidden" }}
-          >
-            <Newsitem
-              newsUrl={news.link}
-              title={news.title ? news.title.slice(0, 50) : "not available"}
-              description={
-                news.description
-                  ? news.description.slice(0, 90)
-                  : "not available"
-              }
-              imageurl={news.image_url ? news.image_url : fallbackimg}
-            />
-          </div>
-        ))}
+    <>
+      <div>
+        <h2 className="text-center">MonkeyNews-Top headline</h2>
+        <div className="row">
+          {article.map((news, index) => (
+            <div
+              className="col-md-4 mb-4"
+              key={news.link}
+              style={{ height: "500px", overflow: "hidden" }}
+            >
+              <Newsitem
+                newsUrl={news.link}
+                title={news.title ? news.title.slice(0, 50) : "not available"}
+                description={
+                  news.description
+                    ? news.description.slice(0, 90)
+                    : "not available"
+                }
+                imageurl={news.image_url ? news.image_url : fallbackimg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="container d-flex justify-content-end mb-3">
+        <button type="button" className="btn btn-dark" onClick={handlenext}>
+          Next 	&rarr;
+        </button>
+      </div>
+    </>
   );
 }
